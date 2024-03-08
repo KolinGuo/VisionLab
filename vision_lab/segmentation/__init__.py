dc765894baae1ede64632756a84902ee38538445
Kolin Guo <ruguo@ucsd.edu>
2024-03-08 14:55:59 -0800

    Add instruction to install segment_anything

--- vision_lab/segmentation/__init__.py
+++ vision_lab/segmentation/__init__.py
@@ -4,5 +4,9 @@ try:
     from .sam import SAM
 except ImportError as e:
     get_logger("VisionLab").warning(f"Failed to import SAM: {e}")
+    get_logger("VisionLab").warning(
+        "Install segment_anything via "
+        "`python3 -m pip install git+https://github.com/facebookresearch/segment-anything.git`"
+    )
 
 # from .mobile_sam import MobileSAM
